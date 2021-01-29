ae9d04a51458bcf718fbffec5a958c785f801206
GGaMErZZ <62007585+GGaMErZZ@users.noreply.github.com>
2021-01-29 17:36:58 +0800

    Multiplayer update (beta)
    the alpha version is lost but the original single player one is saved in my phone for no reason (no i do not code on my phone but i do when i need to)

---
+++ index.js
@@ -0,0 +1,41 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+var players = 0;
+
+io.on('connection', (socket) => {
+	socket.on('move', (pos) => {
+		socket.broadcast.emit("move", pos);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+		players -= 1;
+		socket.emit("leavePlayer", players);
+	});
+
+	socket.on('console', (output) => {
+		console.log(output);
+	});
+
+	socket.emit("syncPlayer", players);
+	socket.broadcast.emit("syncPlayer", players);
+	console.log("user connected")
+	players += 1;
+});
+
+http.listen(3000, () => {
+	console.log('listening on *:3000');
+});
+
+
+
+
+
+
+
